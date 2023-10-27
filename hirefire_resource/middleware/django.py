0323777bfb20054150ae56d9ab41c7fbb6ad64fc
Michael van Rooijen <michael@vanrooijen.io>
2023-10-27 19:30:51 +0200

    WIP

--- hirefire_resource/middleware/django.py
+++ hirefire_resource/middleware/django.py
@@ -6,10 +6,41 @@ from hirefire_resource.resource import Resource
 
 
 class Middleware:
+    """
+    Django middleware for processing requests related to HireFire.
+
+    Acts as a bridge between Django's request/response flow and the
+    base HireFire middleware. It extracts request information from Django's request object
+    and forwards it to the base middleware. If the request is relevant to HireFire,
+    it constructs a response accordingly. Otherwise, it continues the regular request flow.
+
+    Attributes:
+        get_response (function): A reference to the next middleware or view in the processing chain.
+    """
+
     def __init__(self, get_response):
+        """
+        Initialize the middleware with a given get_response function.
+
+        Args:
+            get_response (function): A reference to the next middleware or view in the processing chain.
+        """
         self.get_response = get_response
 
     def __call__(self, request):
+        """
+        Process the incoming Django request.
+
+        It extracts the request information, forwards it to the base HireFire middleware,
+        and either returns a HireFire related response or proceeds with the regular
+        request processing chain.
+
+        Args:
+            request (HttpRequest): The incoming Django request object.
+
+        Returns:
+            HttpResponse: The response to be returned to the client.
+        """
         base_middleware = BaseMiddleware(Resource.configuration)
         request_info = RequestInfo(request.path, request.META)
         response_data = base_middleware.process_request(request_info)
--- hirefire_resource/middleware/flask.py
+++ hirefire_resource/middleware/flask.py
@@ -6,11 +6,44 @@ from hirefire_resource.middleware import RequestInfo
 
 
 class Middleware:
+    """
+    Flask middleware for processing requests related to HireFire.
+
+    Acts as a bridge between Flask's request/response flow and the
+    base HireFire middleware. It extracts request information from Flask's request object
+    and forwards it to the base middleware. If the request is relevant to HireFire,
+    it constructs a response accordingly. Otherwise, it continues the regular request flow.
+
+    Attributes:
+        app (Flask): The Flask application instance.
+        original_wsgi_app (WSGI application): The original WSGI application function that this middleware wraps.
+    """
+
     def __init__(self, app):
+        """
+        Initialize the middleware with a given Flask application instance.
+
+        Args:
+            app (Flask): The Flask application instance.
+        """
         self.app = app
         self.original_wsgi_app = app.wsgi_app
 
     def __call__(self, environ, start_response):
+        """
+        Process the incoming WSGI request.
+
+        It extracts the request information, forwards it to the base HireFire middleware,
+        and either returns a HireFire related response or proceeds with the regular
+        request processing chain.
+
+        Args:
+            environ (dict): The WSGI environment dictionary.
+            start_response (function): The WSGI start_response function.
+
+        Returns:
+            Response: The response to be returned to the client in WSGI format.
+        """
         with self.app.request_context(environ):
             middleware = BaseMiddleware(Resource.configuration)
             request_info = RequestInfo(request.path, request.environ)
