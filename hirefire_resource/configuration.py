355cde0fe4863b468bfede699f393d6c9cebf03e
Michael van Rooijen <michael@vanrooijen.io>
2023-10-27 07:19:50 +0200

    WIP

--- hirefire_resource/configuration.py
+++ hirefire_resource/configuration.py
@@ -5,19 +5,54 @@ from hirefire_resource.worker import Worker
 
 
 class InvalidDynoName(Exception):
+    """Exception raised for invalid dyno names."""
     pass
 
 
 class MissingDynoProc(Exception):
+    """Exception raised when proc is missing for a worker dyno."""
     pass
 
 
 class Configuration:
+    """
+    Class responsible for handling dyno configurations.
+
+    Attributes:
+        web (Web): Configuration for web dynos.
+        workers (list): List of worker configurations.
+    """
+
     def __init__(self):
+        """
+        Initializes a new Configuration instance.
+        """
         self.web = None
         self.workers = []
 
     def dyno(self, name, proc=None):
+        """
+        Configure a dyno based on its name and associated process.
+
+        If the dyno name is "web", it configures a web dyno.
+        Otherwise, it configures a worker dyno and ensures its name and process adhere to the requirements.
+
+        Args:
+            name (str): The name of the dyno.
+            proc (str, optional): The associated process for worker dynos.
+
+        Returns:
+            Configuration: The current Configuration instance for chaining.
+
+        Raises:
+            InvalidDynoName: If the provided dyno name is invalid.
+            MissingDynoProc: If proc is missing for a worker dyno.
+
+        Examples:
+            >>> config = Configuration()
+            >>> config.dyno("web")
+            >>> config.dyno("worker", lambda: job_queue_latency("default"))
+        """
         if str(name) == "web":
             self.web = Web()
         else:
