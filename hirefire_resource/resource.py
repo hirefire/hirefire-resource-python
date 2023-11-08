fb6a5bbe6303e0c60d7aa8ed2b1a7640f76ca19e
Michael van Rooijen <michael@vanrooijen.io>
2023-11-08 16:29:13 +0100

    WIP

--- hirefire_resource/resource.py
+++ hirefire_resource/resource.py
@@ -4,10 +4,44 @@ from hirefire_resource.configuration import Configuration
 
 
 class Resource:
+    """
+    The `Resource` class is the main entry point for integrating the `hirefire-resource`
+    package into your application. It provides a configuration interface to define how
+    HireFire should collect, serve, and dispatch metrics, which are essential for the
+    autoscaling decisions made by HireFire for Heroku dynos. Users can configure metrics
+    collection for web and worker dynos.
+
+    This setup should be placed in the part of your Python codebase that is executed
+    during the application's startup.
+
+    Attributes:
+        configuration (Configuration): Holds the current configuration instance for
+                                       HireFire metrics collection and dispatching.
+
+    Example:
+        from hirefire_resource import Resource
+        from hirefire_resource.macro.rq import job_queue_latency
+
+        with Resource.configure() as config:
+            # To collect Request Queue Time metrics for autoscaling `web` dynos:
+            config.dyno("web")
+            # To collect Job Queue Latency metrics for autoscaling `worker` dynos:
+            config.dyno("worker", lambda: job_queue_latency("default"))
+    """
+
     configuration = None
 
     @classmethod
     @contextmanager
     def configure(cls):
-        cls.configuration = Configuration()
+        """
+        A context manager that yields the current configuration to a block, allowing
+        for the configuration of the `hirefire-resource` package. This method should be
+        called from an application's startup script or similar.
+
+        Yields:
+            Configuration: The current or new configuration instance to be modified.
+        """
+        if cls.configuration is None:
+            cls.configuration = Configuration()
         yield cls.configuration
--- hirefire_resource/worker.py
+++ hirefire_resource/worker.py
@@ -1,4 +1,43 @@
 class Worker:
+    """
+    The Worker class is responsible for measuring job queue metrics for various worker
+    libraries and making these metrics available to HireFire's servers.
+
+    This class is initialized with a name and a callable object (proc), which contains
+    the metric measuring logic. The name should match the worker dyno designation in
+    the Procfile, such as 'worker' or 'mailer'. The proc should be a function that returns
+    an integer representing the queue metric, either job queue latency or job queue size,
+    which will then be made available to the servers.
+
+    Attributes:
+        name (str): The name of the worker, corresponding to the dyno name in the Procfile.
+        proc (callable): A callable object that returns an integer representing the queue metric.
+
+    Args:
+        name (str): The name of the worker.
+        proc (callable): A callable object to measure the job queue metric.
+    """
+
     def __init__(self, name, proc):
+        """
+        Initializes a new instance of the Worker class.
+
+        Args:
+            name (str): The name of the worker.
+            proc (callable): A callable object that when called, returns an integer representing
+                             the queue metric.
+        """
         self.name = name
         self.proc = proc
+
+    def call(self):
+        """
+        Executes the callable object passed during initialization and returns its result.
+
+        This method should be called to obtain the current queue metric
+        (job queue latency or job queue size).
+
+        Returns:
+            int: The queue metric result from the executed callable.
+        """
+        return self.proc()
