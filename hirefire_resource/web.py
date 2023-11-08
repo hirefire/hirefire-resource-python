993923544b073d43ba9149ed1b0ea651707bf2e4
Michael van Rooijen <michael@vanrooijen.io>
2023-11-08 12:06:02 +0100

    WIP

--- hirefire_resource/web.py
+++ hirefire_resource/web.py
@@ -5,16 +5,41 @@ import threading
 import time
 from datetime import datetime
 
-
 class Web:
     """
-    Responsible for collecting and dispatching request queue time metrics to the HireFire server.
+    Responsible for collecting and dispatching web metrics to the HireFire servers.
+
+    This class is designed to function efficiently in both non-forked (single-process) and forked (multi-process)
+    web server architectures. In a forked environment, it is recommended to instantiate Web objects within a
+    server's initialization code to ensure that each process maintains its own dispatcher.
+
+    The internal buffer (`_buffer`) is a critical component:
+    - It's a dictionary where keys are timestamps (in seconds since the Epoch), and the values are lists of request queue
+      time metrics recorded at that particular timestamp in milliseconds.
+    - Metrics are batched together if added within the same second, enabling efficient dispatching to the HireFire server.
+    - When metrics are dispatched, the entire buffer is flushed to prevent duplicate data transmission.
+    - Metrics older than the TTL (Time-to-Live) will be discarded, ensuring that only recent and relevant metrics are sent.
+
+    Example buffer contents:
+        {
+            1634367001: [3, 9],
+            1634367002: [10, 12, 8],
+        }
 
     Attributes:
-        DISPATCH_INTERVAL (int): The interval between dispatch attempts in seconds.
-        TIMEOUT (int): The timeout for HTTP requests in seconds.
-        TTL (int): Metrics older than this value (in seconds) will be discarded.
+        DISPATCH_INTERVAL (int): Interval between dispatch attempts in seconds.
+        TIMEOUT (int): Timeout for HTTP requests in seconds.
+        TTL (int): Time-to-live for metrics in seconds. Metrics older than this value will be discarded.
+
+    Raises:
+        NetworkError: If there's a network-related issue that prevents communication with the HireFire server.
+        TimeoutError: If the request to the HireFire server times out.
+        ServerError: If the HireFire server returns a 5xx status code indicating a server-side error.
     """
+
+    DISPATCH_INTERVAL = 5  # Interval between dispatch attempts in seconds.
+    TIMEOUT = 5  # Timeout for HTTP requests in seconds.
+    TTL = 60  # Time-to-live for metrics in seconds.
 
     class NetworkError(Exception):
         """Raised when there's a network-related issue."""
@@ -25,83 +50,83 @@ class Web:
     class ServerError(Exception):
         """Raised when the server returns a 5xx status."""
 
-    # The interval between dispatch attempts in seconds.
-    DISPATCH_INTERVAL = 5
-
-    # The timeout for HTTP requests in seconds.
-    TIMEOUT = 5
-
-    # Metrics older than this value (in seconds) will be discarded.
-    TTL = 60
-
     def __init__(self):
         """
-        Initializes the Web object with default values.
+        Initializes the Web object with default values, preparing it to start collecting and dispatching metrics.
 
-        Attributes:
-            _buffer (dict): A buffer storing timestamps and request metrics.
-            _mutex (threading.Lock): A lock for thread safety.
-            _running (bool): Indicates if the dispatcher thread is running.
+        The buffer is structured to group metrics by the second in which they were recorded, allowing
+        efficient batch dispatching. The buffer will only retain recent metrics as defined by the TTL value.
         """
-        # buffer is a dict where the keys are timestamps (in seconds
-        # since the Epoch) and the values are arrays of request queue
-        # time metrics that have been added at that particular timestamp
-        # on a per-request basis.
-        #
-        # For instance:
-        # {
-        #   1634367001 => [3, 9],
-        #   1634367002 => [10, 12, 8]
-        # }
-        #
-        # The purpose of this structure is to batch metrics added at the
-        # same second together, allowing for more efficient dispatching
-        # to the HireFire server.
-        #
-        # When metrics are dispatched to the server, the entire buffer
-        # is flushed, ensuring that data isn't sent more than
-        # once. Metrics older than a certain time (as defined by the TTL
-        # constant) will be discarded if not dispatched in time,
-        # ensuring that only recent and relevant metrics are sent.
-        self._buffer = {}
-        self._mutex = threading.Lock()
-        self._running = False
+        self._buffer = {}  # Stores metrics grouped by timestamp of recording.
+        self._mutex = threading.Lock()  # Ensures thread-safe access to the buffer.
+        self._running = False  # Indicates if the dispatcher thread is running.
+        self._thread = None  # Will hold the dispatcher thread once started.
 
     def start(self):
-        """Starts the web metrics dispatcher thread."""
+        """
+        Starts the dispatcher in a separate thread to continuously dispatch web metrics to the HireFire servers.
+
+        If the dispatcher is already running, this method will have no effect. After starting, the dispatcher
+        logs an informational message indicating its state.
+
+        Example:
+            web = Web()
+            web.start()
+        """
         with self._mutex:
             if self._running:
                 return
             self._running = True
+
         print("[HireFire] Starting web metrics dispatcher.")
-        self.thread = threading.Thread(target=self._run)
-        self.thread.start()
+        self._thread = threading.Thread(target=self._run)
+        self._thread.start()
 
     def stop(self):
-        """Stops the web metrics dispatcher thread."""
+        """
+        Stops the dispatcher thread, ensuring that no further metrics are dispatched to the HireFire server.
+
+        This method waits for the dispatcher's thread to complete before marking it as stopped.
+        After stopping, the buffer is cleared and an informational message is logged.
+
+        Example:
+            web = Web()
+            web.start()
+            # ... some time later ...
+            web.stop()
+        """
         with self._mutex:
             if not self._running:
                 return
             self._running = False
-        self.thread.join(self.TIMEOUT)
+
+        if self._thread:
+            self._thread.join(self.TIMEOUT)
+            self._thread = None
+
+        self._buffer.clear()
         print("[HireFire] Web metrics dispatcher stopped.")
 
     def running(self):
         """
-        Returns the running status of the web metrics dispatcher.
+        Checks if the dispatcher is currently running.
 
         Returns:
-            bool: The current running status of the dispatcher.
+            bool: True if the dispatcher is running, False otherwise.
         """
         with self._mutex:
             return self._running
 
     def add_to_buffer(self, value):
         """
-        Adds a value to the buffer with the current timestamp.
+        Adds a value to the buffer, associating it with the current timestamp.
 
         Args:
-            value (int): The value to be added to the buffer.
+            value (int): The request queue time in milliseconds.
+
+        Example:
+            web = Web()
+            web.add_to_buffer(150)
         """
         with self._mutex:
             timestamp = int(datetime.now().timestamp())
@@ -109,18 +134,25 @@ class Web:
 
     def flush(self):
         """
-        Flushes the buffer and returns its content.
+        Flushes the buffer, returning its contents, and creates a new empty buffer.
 
         Returns:
-            dict: The current content of the buffer.
+            dict: The buffer's contents before the flush.
         """
         with self._mutex:
-            buffer_copy = self._buffer.copy()
-            self._buffer.clear()
-            return buffer_copy
+            buffer = self._buffer
+            self._buffer = {}
+            return buffer
 
     def dispatch(self):
-        """Dispatches the buffer to the server and handles any exceptions."""
+        """
+        Dispatches the metrics buffer to the HireFire servers.
+
+        If the buffer is not empty, it submits the buffer contents. In case of an exception, it attempts to
+        repopulate the buffer with the undelivered metrics.
+
+        Note: This method does not return any value.
+        """
         buffer = self.flush()
         if buffer:
             try:
@@ -131,21 +163,21 @@ class Web:
 
     def _run(self):
         """
-        The main loop for the dispatcher thread, dispatching metrics at regular intervals.
+        The dispatcher's main loop that triggers the dispatch of metrics at set intervals.
         """
         while self.running():
             try:
                 self.dispatch()
             except Exception as e:
                 print(f"[HireFire] Unexpected error during dispatch: {str(e)}")
-                time.sleep(self.DISPATCH_INTERVAL)
+            time.sleep(self.DISPATCH_INTERVAL)
 
     def _repopulate_buffer(self, buffer):
         """
-        Repopulates the buffer with valid (non-expired) metrics if dispatching fails.
+        Repopulates the buffer with valid metrics if a dispatch attempt fails.
 
         Args:
-            buffer (dict): The buffer containing metrics to be repopulated.
+            buffer (dict): The buffer containing metrics to repopulate.
         """
         now = int(datetime.now().timestamp())
         with self._mutex:
@@ -155,13 +187,18 @@ class Web:
 
     def _submit_buffer(self, buffer):
         """
-        Submits the buffer to the HireFire server.
+        Submits the buffer to the HireFire servers via an HTTP POST request.
 
         Args:
             buffer (dict): The buffer containing metrics to be dispatched.
 
         Returns:
-            bool: True if the submission is successful, otherwise raises an error.
+            bool: True if the submission is successful, False otherwise.
+
+        Raises:
+            NetworkError: For network-related issues.
+            TimeoutError: If the request to the server times out.
+            ServerError: If the server returns a 5xx status.
         """
         buffer_string = json.dumps(buffer)
         headers = {
@@ -169,10 +206,8 @@ class Web:
             "User-Agent": "HireFire Agent (Python)",
             "HireFire-Token": os.environ.get("HIREFIRE_TOKEN", ""),
         }
-        connection = http.client.HTTPSConnection(
-            "logdrain.hirefire.io", timeout=self.TIMEOUT
-        )
 
+        connection = http.client.HTTPSConnection("logdrain.hirefire.io", timeout=self.TIMEOUT)
         try:
             connection.request("POST", "/", buffer_string, headers)
             response = connection.getresponse()
@@ -180,13 +215,9 @@ class Web:
             if 200 <= response.status < 300:
                 return True
             elif 500 <= response.status < 600:
-                raise self.ServerError(
-                    f"Server returned {response.status}: {response.reason}"
-                )
+                raise self.ServerError(f"Server returned {response.status}: {response.reason}")
             else:
-                raise self.NetworkError(
-                    f"Request failed with {response.status}: {response.reason}"
-                )
+                raise self.NetworkError(f"Request failed with {response.status}: {response.reason}")
 
         except http.client.HTTPException as e:
             raise self.NetworkError(f"HTTP error occurred: {str(e)}")
