3a3adbeaa5b0b182b9fbc721db164c10dec4ed35
Michael van Rooijen <michael@vanrooijen.io>
2023-11-08 16:52:02 +0100

    WIP

--- hirefire_resource/configuration.py
+++ hirefire_resource/configuration.py
@@ -1,68 +1,65 @@
-import re
-
-from hirefire_resource.web import Web
-from hirefire_resource.worker import Worker
-
-
-class InvalidDynoName(Exception):
-    """Exception raised for invalid dyno names."""
-
-
-class MissingDynoProc(Exception):
-    """Exception raised when proc is missing for a worker dyno."""
-
-
 class Configuration:
     """
-    Class responsible for handling dyno configurations.
+    Responsible for handling HireFire configuration within an application. It allows defining
+    the collection of metrics for web and worker dynos as required for autoscaling decisions
+    by HireFire for Heroku dynos.
 
     Attributes:
-        web (Web): Configuration for web dynos.
-        workers (list): List of worker configurations.
+        web (Web or None): Instance of the Web class responsible for collecting and dispatching web metrics to
+                           HireFire's servers. It is set when calling `dyno("web")`.
+        workers (list of Worker): List of Worker class instances, each configured with a dyno name
+                                  and a proc that defines its job queue metric measurement logic.
+
+    Raises:
+        InvalidDynoName: Raised if the provided dyno name does not conform to the Procfile naming restrictions.
+        MissingDynoProc: Raised if a required proc is not provided for a worker dyno configuration.
     """
 
     def __init__(self):
         """
-        Initializes a new Configuration instance.
+        Initializes the Configuration instance, preparing it to handle web and worker dyno configurations.
         """
         self.web = None
         self.workers = []
 
     def dyno(self, name, proc=None):
         """
-        Configure a dyno based on its name and associated process.
+        Configures Web and Worker objects.
 
-        If the dyno name is "web", it configures a web dyno.
-        Otherwise, it configures a worker dyno and ensures its name and process adhere to the requirements.
+        The proc is ignored for Web objects, as the Web object as it is not used for collecting
+        web metrics.
+
+        The proc is required for Worker objects as it should return the job queue latency or
+        job queue size metric.
 
         Args:
-            name (str): The name of the dyno.
-            proc (str, optional): The associated process for worker dynos.
+            name (str): The name of the dyno as declared in the Procfile.
+            proc (callable, optional): The proc for worker dynos that returns an integer representing
+                                       the job queue metric. It must be defined for worker dynos.
 
         Returns:
-            Configuration: The current Configuration instance for chaining.
+            Configuration: The current Configuration instance for method chaining.
 
         Raises:
-            InvalidDynoName: If the provided dyno name is invalid.
-            MissingDynoProc: If proc is missing for a worker dyno.
+            InvalidDynoName: If the dyno name does not match Procfile naming restrictions.
+            MissingDynoProc: If a proc is missing for a worker dyno.
 
         Examples:
             >>> config = Configuration()
+            # For the collection and dispatching of request queue time metrics
             >>> config.dyno("web")
+            # For the collection and serving of job queue metrics
             >>> config.dyno("worker", lambda: job_queue_latency("default"))
         """
-        if str(name) == "web":
+        if name == "web":
             self.web = Web()
-        else:
-            if not re.match(r"^[a-zA-Z][a-zA-Z0-9_]{0,29}$", str(name)):
-                raise InvalidDynoName(
-                    f"Invalid name for {self.__class__}#dyno({name}). "
-                    "Ensure it matches the Procfile process name "
-                    "(i.e. web, worker, mailer, low, high, critical, etc)."
-                )
-            elif proc is None:
+        elif re.match(r"^[a-zA-Z][a-zA-Z0-9_]{0,29}$", name):
+            if proc is None:
                 raise MissingDynoProc("proc must be defined for worker dynos")
-            else:
-                self.workers.append(Worker(name, proc))
+            self.workers.append(Worker(name, proc))
+        else:
+            raise InvalidDynoName(
+                f"Invalid dyno name: '{name}'. Must conform to Procfile naming restrictions."
+            )
 
         return self
