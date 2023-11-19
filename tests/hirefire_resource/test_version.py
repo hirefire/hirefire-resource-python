fb74b2e4da5b67d13a3f8b53873aa348119e999a
Michael van Rooijen <michael@vanrooijen.io>
2023-11-19 12:08:52 +0100

    WIP

--- tests/hirefire_resource/test_version.py
+++ tests/hirefire_resource/test_version.py
@@ -1,10 +1,7 @@
+import re
+
 from hirefire_resource.version import VERSION
 
+
 def test_version():
-    assert VERSION
-    assert isinstance(VERSION, str)
-    assert "." in VERSION
-    assert VERSION.count(".") == 2
-    assert VERSION.split(".")[0].isdigit()
-    assert VERSION.split(".")[1].isdigit()
-    assert VERSION.split(".")[2].isdigit()
+    assert re.match(r"\d+\.\d+\.\d+", VERSION)
