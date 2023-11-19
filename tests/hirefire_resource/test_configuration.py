33a3790c0dcf8cac615aa7c0a1e16e51a45dc506
Michael van Rooijen <michael@vanrooijen.io>
2023-11-19 14:29:44 +0100

    WIP

--- tests/hirefire_resource/test_configuration.py
+++ tests/hirefire_resource/test_configuration.py
@@ -9,8 +9,12 @@ def test_web():
     assert isinstance(config.web, Web)
 
 
-def test_worker():
+def test_workers():
     config = Configuration()
     config.dyno("worker", lambda: 1.23)
-    assert isinstance(config.workers[0], Worker)
-    assert len(config.workers) == 1
+    config.dyno("mailer", lambda: 2.46)
+    assert len(config.workers) == 2
+    assert config.workers[0].name == "worker"
+    assert config.workers[0].value() == 1.23
+    assert config.workers[1].name == "mailer"
+    assert config.workers[1].value() == 2.46
