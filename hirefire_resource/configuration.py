2b749c663e06a6fe1624d5e13c0a90e3f926d90e
Michael van Rooijen <michael@vanrooijen.io>
2023-12-18 16:13:21 +0100

    WIP

--- hirefire_resource/configuration.py
+++ hirefire_resource/configuration.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 from hirefire_resource.web import Web
 from hirefire_resource.worker import Worker
@@ -8,7 +9,21 @@ class Configuration:
     def __init__(self):
         self.web = None
         self.workers = []
-        self.logger = logging.getLogger()
+        self.logger = self._init_logger()
+
+    def _init_logger(self):
+        logger = logging.getLogger("hirefire_resource")
+        logger.setLevel(logging.INFO)
+
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+
+        formatter = logging.Formatter("%(message)s")
+        handler.setFormatter(formatter)
+
+        logger.addHandler(handler)
+
+        return logger
 
     def dyno(self, name, proc=None):
         if name == "web":
